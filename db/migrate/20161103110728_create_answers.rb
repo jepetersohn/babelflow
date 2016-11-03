ed2325645470bb7af82b8de3bd5b14bfec03ec55
Adam Vera & Sam Hopkins <pair+ajvera.hopkins571@devbootcamp.com>
2016-11-03 11:18:38 -0500

    write migrations

---
+++ db/migrate/20161103110728_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.integer :question_id, { null: false }
+      t.integer :responder_id, { null: false }
+      t.text  :body, { null: false}
+      t.boolean :best, { default: false }
+
+      t.timestamps(null:false)
+    end
+  end
+end
