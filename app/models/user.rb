373517ab1e629ccb44390a43ac719d4e1958f67d
Corbin Via & Joan Petersohn <pair+corbin1987.jepetersohn@devbootcamp.com>
2016-11-03 16:09:43 -0500

    Add helper for user login, add routes and views for user login and new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,25 @@ class User < ActiveRecord::Base
 
 
   def validate_password_length
-
+    if @raw_password.nil?
+      errors.add(:password, "is required")
+    elsif @raw_password.length < 4
+      errors.add(:password, "must be at least 4 characters")
+    end
   end
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+    password = BCrypt::Password.create(new_password)
+    self.hashed_password = password
+  end
+
+  def authenticate(email, password)
+    self.password == password && self.email == email
+  end
+
 end
