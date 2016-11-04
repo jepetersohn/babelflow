18df8ebd73204b33fcdff47f61d87b999cc7fe53
Joan Petersohn <jepetersohn@users.noreply.github.com>
2016-11-04 14:31:55 -0500

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   addQuestionHandler();
+  handleQuestionVote();
+  // handleLoggingOut();
 });
 
 var addQuestionHandler = function() {
@@ -21,3 +23,41 @@ var addQuestionHandler = function() {
     });
   });
 };
+
+function handleQuestionVote(){
+  $('.vote').on("submit", function(event) {
+    event.preventDefault();
+    var url = $(this).attr('action');
+    var vote_value = $(this).children('button').attr('value');
+    var $form = $(this)
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: {vote: vote_value}
+    })
+    .done(function(response) {
+      var new_value = 'Vote score:' + ' ' + response
+      $form.closest('.question-votes').children('#score').text(new_value);
+    });
+  });
+};
+
+  // handleLoggingOut();
+
+
+// function handleLoggingOut() {
+//   $('.logout').on('click', function(event) {
+//   event.preventDefault();
+//  var logoutUrl = $(this).attr('action');
+//   var request = $.ajax({
+//     method: "DELETE",
+//     url: logoutUrl
+//   })
+//   request.done(function() {
+//     $(".logged_in_nav").remove();
+//     $(".not_logged_in_nav").add();
+    //also tried show/hide. same results.
+    //also need to account for what to do about logging out on a page other than the main page...like, hiding the comment/answer buttons etc
+//   })
+//  });
+// };
