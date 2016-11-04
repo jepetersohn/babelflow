3a28f7be7105aec31eeaaca8768be276465c6bee
Sam Hopkins <hopkins571@users.noreply.github.com>
2016-11-04 16:56:12 -0500

    Revert "Voting"

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -9,16 +9,3 @@ post '/questions/:id/votes' do
   end
 
 end
-
-post '/questions/:id/answers/:answer_id/votes' do
-  @question = Question.find_by(id: params[:id])
-  @answer = Answer.find_by(id: params[:answer_id])
-  if request.xhr?
-    @vote = Vote.create(votable_type: "Answer", votable_id: @answer.id, voter_id: current_user.id, vote: params[:vote])
-    @answer.votes.pluck(:vote).sum.to_s
-  else
-    @vote = Vote.create(votable_type: "Answer", votable_id: @answer.id, voter_id: current_user.id, vote: params[:vote].to_i)
-    redirect "/questions/#{@question.id}"
-  end
-
-end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   addQuestionHandler();
   handleQuestionVote();
-  handleAnswerVote();
   // handleLoggingOut();
 });
 
@@ -40,24 +39,6 @@ function handleQuestionVote(){
     .done(function(response) {
       var new_value = 'Vote score:' + ' ' + response
       $form.closest('.question-votes').children('#score').text(new_value);
-    });
-  });
-};
-
-function handleAnswerVote(){
-  $('.vote').on("submit", function(event) {
-    event.preventDefault();
-    var url = $(this).attr('action');
-    var vote_value = $(this).children('button').attr('value');
-    var $form = $(this)
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: {vote: vote_value}
-    })
-    .done(function(response) {
-      var new_value = 'Vote score:' + ' ' + response
-      $form.closest('.answer-votes').children('#score').text(new_value);
     });
   });
 };
