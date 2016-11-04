e9021908765267dd4b551d0ac88e5cc5e0600873
Corbin Via <email": null>
2016-11-04 11:44:55 -0500

    Declare route for posting a new question, and it works OMG yay

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -11,10 +11,11 @@ post '/questions' do
   if logged_in?
     @author = current_user
     @question = Question.new(params[:question])
-    if request.xhr?
-      ":)"
+    @question.author = author
+    if @question.save #request.xhr?
+      redirect "/questions/#{@question.id}"
     else
-      status 403
+      status 500
     end
   else
     status 403
