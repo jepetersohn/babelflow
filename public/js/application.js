a5a8ad42e2834625afd070f42e7e8c84208269ed
Corbin Via <email": null>
2016-11-04 14:14:30 -0500

    Remove comments

--- public/js/application.js
+++ public/js/application.js
@@ -6,17 +6,14 @@ var addQuestionHandler = function() {
   $("#question-post-form").on("submit", function(event) {
     event.preventDefault();
     var form = $("#question-post-form");
-    // console.log(form);
     var listToAppend = $("#question-list");
     var data = form.serializeArray();
     $.ajax({
       url: '/questions',
       type: 'POST',
-      // dataType: 'json',
       data: data
     })
     .done(function(response) {
-      console.log(response);
       listToAppend.append(response);
     })
     .fail(function() {
