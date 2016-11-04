664ef7b3f451b930c5dc2245ba34f8d499af7bec
alanahanson <jepetersohn@gmail.com>
2016-11-04 12:27:30 -0500

    here's the fully functional non-ajax logout

--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  handleLoggingOut();
+  // handleLoggingOut();
 });
 
-function handleLoggingOut() {
-  $('.logout').on('click', function(event) {
-  event.preventDefault();
- var logoutUrl = $(this).attr('action');
-  var request = $.ajax({
-    method: "DELETE",
-    url: logoutUrl
-  })
-  request.done(function() {
-    $(".logged_in_nav").remove();
-    $(".not_logged_in_nav").add();
+// function handleLoggingOut() {
+//   $('.logout').on('click', function(event) {
+//   event.preventDefault();
+//  var logoutUrl = $(this).attr('action');
+//   var request = $.ajax({
+//     method: "DELETE",
+//     url: logoutUrl
+//   })
+//   request.done(function() {
+//     $(".logged_in_nav").remove();
+//     $(".not_logged_in_nav").add();
     //also tried show/hide. same results.
     //also need to account for what to do about logging out on a page other than the main page...like, hiding the comment/answer buttons etc
   })
