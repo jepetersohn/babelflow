cdca28cdf68e44252b546e20757dfd0ff8a7700a
Sam Hopkins <hopkins.571@gmail.com>
2016-11-05 15:17:20 -0500

    refactor votes route to prep for future vote limits

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,10 +1,9 @@
 post '/questions/:id/votes' do
   @question = Question.find_by(id: params[:id])
+  @vote = Vote.create(votable_type: "Question", votable_id: @question.id, voter_id: current_user.id, vote: params[:vote])
   if request.xhr?
-    @vote = Vote.create(votable_type: "Question", votable_id: @question.id, voter_id: current_user.id, vote: params[:vote])
     @question.votes.pluck(:vote).sum.to_s
   else
-    @vote = Vote.create(votable_type: "Question", votable_id: @question.id, voter_id: current_user.id, vote: params[:vote].to_i)
     redirect "/questions/#{@question.id}"
   end
 
