951e229548a4e08fc9eb8470182c18bf9105f153
Sam Hopkins <hopkins.571@gmail.com>
2016-11-06 18:12:17 -0600

    implement vote limits

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,23 +1,38 @@
 post '/questions/:id/votes' do
-  @question = Question.find_by(id: params[:id])
-  @vote = Vote.create(votable_type: "Question", votable_id: @question.id, voter_id: current_user.id, vote: params[:vote])
-  if request.xhr?
-    @question.votes.pluck(:vote).sum.to_s
+  if logged_in?
+    @question = Question.find_by(id: params[:id])
+    @question.votes.where(voter: current_user).destroy_all
+    @vote = Vote.create(votable_type: "Question", votable_id: @question.id, voter_id: current_user.id, vote: params[:vote])
+    if request.xhr?
+      @question.votes.pluck(:vote).sum.to_s
+    else
+      redirect "/questions/#{@question.id}"
+    end
   else
-    redirect "/questions/#{@question.id}"
+    if request.xhr?
+      status 403
+    else
+      redirect "/"
+    end
   end
-
 end
 
 post '/questions/:id/answers/:answer_id/votes' do
-  @question = Question.find_by(id: params[:id])
-  @answer = Answer.find_by(id: params[:answer_id])
-  if request.xhr?
+  if logged_in?
+    @question = Question.find_by(id: params[:id])
+    @answer = Answer.find_by(id: params[:answer_id])
+    @answer.votes.where(voter: current_user).destroy_all
     @vote = Vote.create(votable_type: "Answer", votable_id: @answer.id, voter_id: current_user.id, vote: params[:vote])
-    @answer.votes.pluck(:vote).sum.to_s
+    if request.xhr?
+      @answer.votes.pluck(:vote).sum.to_s
+    else
+      redirect "/questions/#{@question.id}"
+    end
   else
-    @vote = Vote.create(votable_type: "Answer", votable_id: @answer.id, voter_id: current_user.id, vote: params[:vote].to_i)
-    redirect "/questions/#{@question.id}"
+    if request.xhr?
+      status 403
+    else
+      redirect "/"
+    end
   end
-
 end
