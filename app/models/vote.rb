8e2181057cbdf25bc31554b9b3e1d42fcc6a178c
Sam Hopkins <hopkins.571@gmail.com>
2016-11-06 22:40:00 -0600

    vote value must be between -1 and 1

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
   belongs_to :voter, { class_name: "User" }
   validates :votable, { presence: true }
+  validates :vote, { numericality: { less_than_or_equal_to: 1,
+  																	greater_than_or_equal_to: -1 }}
 end
